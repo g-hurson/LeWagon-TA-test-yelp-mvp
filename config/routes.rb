a767117e05c98f945f771027ac581e43ba0456b5
Geraldine Hurson <geraldinehurson@gmail.com>
2024-06-19 20:39:29 +0200

    --wip-- [skip ci]

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,6 @@ Rails.application.routes.draw do
   # 1- we can see the list of all restaurants : GET "restaurants"
   # 2- we can add a new restaurant, and go to its show view : GET "restaurants/new", POST "restaurants"
   # 3- we can see the details of a restaurant, with all its related reviews : GET "restaurants/38"
-  # 4- we can add a new review to a restaurant : GET "restaurants/38/reviews/new", POST "restaurants/38/reviews"
+  # 4- we can add a new review to a restaurant : POST "restaurants/38/reviews"
 
 end
